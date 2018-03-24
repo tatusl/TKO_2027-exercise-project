c9b6af6c7eefd8cab0172c9b42fb95ff56bee787
Tatu Seppä-Lassila <tatu.seppa-lassila@polarsquad.com>
2018-03-24 18:18:41 +0200

    [kube-common] Use vpc remote state

--- terraform/kube-common/main.tf
+++ terraform/kube-common/main.tf
@@ -2,33 +2,57 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket = "tatusl-ep-terraform-remote-state"
+    key    = "vpc_${var.env}.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "bastion" {
+  backend = "s3"
+
+  config {
+    bucket = "tatusl-ep-terraform-remote-state"
+    key    = "bastion_${var.env}.tfstate"
+    region = "eu-west-1"
+  }
+}
+
 resource "aws_key_pair" "kube_cluster_node" {
   key_name   = "kube_cluster_node"
   public_key = "${file("files/cluster_node_public_key.pem.pub")}"
 }
 
+# Refactor to security_group_rules
 resource "aws_security_group" "kube_cluster_node" {
   name        = "kube_cluster_nodes"
-  description = "Allow ssh from own ip, allow all out, allow all within this security group"
-  vpc_id      = "vpc-ad4434c9"                                                               # Account default VPC
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "86.115.198.116/32",
-      "80.222.55.12/32",
-      "193.65.194.73/32" # pqsd office
-    ]
-  }
+  description = "Allow all out, allow all in within this security group"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
 
   ingress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
     self      = true
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    security_groups = ["${data.terraform_remote_state.bastion.bastion_security_group_id}"]
+  }
+
+  # TODO: Kubernetes API access (port 6443)should be only allowed for master
+  ingress {
+    from_port = 6443
+    to_port   = 6443
+    protocol  = "tcp"
+    security_groups = ["${data.terraform_remote_state.bastion.bastion_security_group_id}"]
   }
 
   egress {
